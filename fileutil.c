c432a6acd337b7ae7c71fdf64cb7ba724875774d
Seldon Lee <54562472+Seldon1224@users.noreply.github.com>
2022-12-12 15:08:12 +0800

    Update fileutil.c

--- fileutil.c
+++ fileutil.c
@@ -3,6 +3,350 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <iostream>
+using namespace std;
+
+#define RED 1
+#define BLACK 0
+class node
+{
+public:
+	int data;
+	node *rchild, *lchild;
+	node *parent;
+	bool color; // 0=black,1=red;
+};
+class tree
+{
+public:
+	node *root;
+	node *nil;
+	tree()
+	{
+		root = nullptr;
+	}
+
+	void insert(const int &a)
+	{
+		node *&bt = root;
+		node *s = new node;
+		s->data = a;
+		s->color = true; // ²åÈë½ÚµãÎªºìÉ«
+		s->lchild = nullptr;
+		s->rchild = nullptr;
+		s->parent = nullptr;
+		// node* &bt = root;
+		if (bt == nullptr)
+		{
+			bt = s;
+			bt->color = false;
+			bt->parent = bt;
+			bt->lchild = nullptr;
+			bt->rchild = nullptr;
+		}
+		else
+		{
+			// ÕÒµ½Òª²åÈëµÄÎ»ÖÃ
+			node *curr = root;
+			while (curr->lchild != nullptr | curr->rchild != nullptr)
+			{
+				if (a < curr->data)
+					curr = curr->lchild;
+				else
+					curr = curr->rchild;
+			}
+
+			// ²åÈës½Úµã
+			s->parent = curr;
+			if (a < curr->data)
+				curr->lchild = s;
+			else
+				curr->rchild = s;
+
+			// ¸üÐÂºìºÚÊ÷£¬±£Ö¤Âú×ãÌØÐÔ
+			while (curr->color == RED && curr->parent != nullptr)
+			{
+				bool isRight = (curr == curr->parent->rchild);
+				node *uncle;
+				if (isRight)
+					uncle = curr->parent->lchild;
+				else
+					uncle = curr->parent->rchild;
+
+				if (uncle == nullptr) // Èç¹ûuncle½ÚµãÎª¿Õ
+				{
+					curr->color = BLACK;
+					curr->parent->color = RED;
+					if (uncle == curr->parent->rchild)
+					{
+						rightRotate(curr->parent);
+					}
+					else
+					{
+						leftRotate(curr->parent);
+					}
+					break;
+				}
+				else if (uncle->color == RED) // Èç¹ûuncle½ÚµãÊÇºì
+				{
+					curr->color = BLACK;
+					uncle->color = BLACK;
+					curr->parent->color = RED;
+					curr = curr->parent;
+				}
+				else
+				{
+					curr->color = BLACK;
+					curr->parent->color = RED;
+
+					if (isRight)
+					{
+						if (s == curr->lchild)
+						{
+							rightRotate(curr);
+							curr = s;
+						}
+						leftRotate(curr->parent);
+					}
+					else
+					{
+						if (s == curr->rchild)
+						{
+							leftRotate(curr);
+							curr = s;
+						}
+						rightRotate(curr->parent);
+					}
+				}
+				root->color = BLACK;
+			}
+		}
+	}
+
+	bool remove(const int &key)
+	{
+		// ÕÒµ½ÒªÉ¾³ýµÄ½ÚµãÎ»ÖÃ
+		node *curr = root;
+		while (curr->lchild != nullptr | curr->rchild != nullptr)
+		{
+			if (curr->data == key)
+				break;
+
+			if (key >= curr->data)
+				curr = curr->rchild;
+			else
+				curr = curr->lchild;
+		}
+		if (curr->data != key)
+			return 0;
+
+		this->removeNode(curr);
+		return 1;
+	}
+
+	void removeNode(node *rbnode)
+	{
+		if (rbnode->color == RED)
+		{
+			if (rbnode->lchild != nullptr && rbnode->rchild != nullptr)
+			{
+				// rbnodeÖ»ÓÐlchildÒÔ¼°rchild
+				// ÕÒµ½×î×óµÄºó¼Ì½Úµã£¬ÓÃ´Ë½ÚµãÌæ»»rbnode
+				node *successor = rbnode->rchild;
+				while (successor->lchild != nullptr)
+					successor = successor->lchild;
+				rbnode->data = successor->data;
+				this->removeNode(successor);
+			}
+			else if (rbnode->lchild != nullptr)
+			{
+				// rbnodeÖ»ÓÐlchildÊ±
+				rbnode->data = rbnode->lchild->data;
+				rbnode->color = rbnode->lchild->color;
+				this->removeNode(rbnode->lchild);
+			}
+			else if (rbnode->rchild != nullptr)
+			{
+				// rbnodeÖ»ÓÐrchildÊ±
+				rbnode->data = rbnode->rchild->data;
+				rbnode->color = rbnode->rchild->color;
+				this->removeNode(rbnode->rchild);
+			}
+			else
+			{
+				// rbnodeÃ»ÓÐº¢×Ó½Úµã
+				if (rbnode->parent == nullptr)
+				{
+					free(rbnode);
+					root = nullptr;
+					return;
+				}
+
+				if (rbnode->parent->lchild == rbnode)
+					rbnode->parent->lchild = nullptr;
+				else
+					rbnode->parent->rchild = nullptr;
+
+				free(rbnode);
+			}
+		}
+		else
+		{
+			if (rbnode->lchild != nullptr && rbnode->rchild != nullptr)
+			{
+				// rbnodeÖ»ÓÐlchildÒÔ¼°rchild
+				// ÕÒµ½×î×óµÄºó¼Ì½Úµã£¬ÓÃ´Ë½ÚµãÌæ»»rbnode
+				node *successor = rbnode->rchild;
+				while (successor->lchild != nullptr)
+					successor = successor->lchild;
+				rbnode->data = successor->data;
+				this->removeNode(successor);
+			}
+			else if (rbnode->lchild != nullptr)
+			{
+				// rbnodeÖ»ÓÐlchildÊ±
+				rbnode->data = rbnode->lchild->data;
+				this->removeNode(rbnode->lchild);
+			}
+			else if (rbnode->rchild != nullptr)
+			{
+				// rbnodeÖ»ÓÐrchildÊ±
+				rbnode->data = rbnode->rchild->data;
+				this->removeNode(rbnode->rchild);
+			}
+			else
+			{
+				// rbnodeÃ»ÓÐº¢×Ó½Úµã£¬¼´ÎªÒ¶×Ó½Úµã
+				// ×÷ÎªÑÕÉ«Îªblack½Úµã£¬ÐèÒªfixupÐÞÕý£¬±£Ö¤ºìºÚÊ÷ÌØÐÔ
+				if (rbnode->parent == nullptr)
+				{
+					// 'rbnode'ÎªRoot
+					free(rbnode);
+					root = nullptr;
+					return;
+				}
+				// rbnodeÊÇ¸¸½ÚµãµÄ×óº¢×Ó
+				if (rbnode->parent->lchild == rbnode)
+				{
+					rbnode->parent->lchild = nullptr;
+					if (rbnode->parent->rchild != nullptr && rbnode->parent->rchild->color == RED)
+					{
+						rbnode->parent->rchild->color = BLACK;
+						leftRotate(rbnode->parent);
+					}
+				}
+				else // rbnodeÊÇ¸¸½ÚµãµÄÓÒº¢×Ó
+				{
+					rbnode->parent->rchild = nullptr;
+					if (rbnode->parent->lchild != nullptr && rbnode->parent->lchild->color == RED)
+					{
+						rbnode->parent->lchild->color = BLACK;
+						rightRotate(rbnode->parent);
+					}
+				}
+
+				if (rbnode->parent->lchild == nullptr && rbnode->parent->rchild == nullptr && rbnode->parent->parent != nullptr)
+				{
+					// 'rbnode->parent->parent'ÎªRED
+					rightRotate(rbnode->parent->parent);
+				}
+
+				free(rbnode);
+			}
+		}
+	}
+
+	// ÒÔrbnodeÎªÖÐÐÄ£¬½øÐÐ×óÐý
+	void leftRotate(node *rbnode)
+	{
+		node *temp = rbnode->rchild;
+
+		// ½øÐÐ×óÐý²Ù×÷
+		rbnode->rchild = temp->lchild;
+		if (temp->lchild != nullptr)
+			temp->lchild->parent = rbnode;
+		temp->lchild = rbnode;
+		temp->parent = rbnode->parent;
+		rbnode->parent = temp;
+
+		// ¸üÐÂparent½Úµã
+		if (root == rbnode)
+		{
+			root = temp;
+			return;
+		}
+		if (temp->parent->lchild == rbnode)
+			temp->parent->lchild = temp;
+		else
+			temp->parent->rchild = temp;
+	}
+
+	// ÒÔrbnodeÎªÖÐÐÄ£¬½øÐÐÓÒÐý²Ù×÷
+	void rightRotate(node *rbnode)
+	{
+		node *temp = rbnode->lchild;
+
+		// ½øÐÐÓÒÐý²Ù×÷
+		rbnode->lchild = temp->rchild;
+		if (temp->rchild != nullptr)
+			temp->rchild->parent = rbnode;
+		temp->rchild = rbnode;
+		temp->parent = rbnode->parent;
+		rbnode->parent = temp;
+
+		// ¸üÐÂparent½Úµã
+		if (root == rbnode)
+		{
+			root = temp;
+			return;
+		}
+		if (temp->parent->lchild == rbnode)
+			temp->parent->lchild = temp;
+		else
+			temp->parent->rchild = temp;
+	}
+
+	void PreOrder(node *&bt)
+	{
+		if (bt == nullptr)
+		{
+			return;
+		}
+		else
+		{
+			if (bt == root)
+			{
+				cout << bt->data << "\\" << bt->color;
+			}
+			else
+			{
+				cout << " " << bt->data << "\\" << bt->color;
+			}
+		}
+		PreOrder(bt->lchild);
+		PreOrder(bt->rchild);
+	}
+};
+
+int main()
+{
+	int n;
+	cin >> n;
+
+	tree T;
+
+	while (n--)
+	{
+		int a;
+		cin >> a;
+		T.insert(a);
+	}
+	T.PreOrder(T.root);
+
+	cout << endl;
+	return 0;
+}
+
 char *default_file_name = "sample.txt";
 char *source_file_name = NULL;
 char *target_file_name = NULL;
